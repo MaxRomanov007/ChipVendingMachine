84975e35a1c608001e829745c9373e4f71c79a02
MaxRomanov007 <maksipapa999@gmail.com>
2025-09-07 08:23:13 +0300

    added chips, trying to make chips can fall

---
+++ App/Domain/Models/Chips.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace App.Domain.Models;
+
+public class Chips : IDisposable
+{
+    public Bitmap? Image { get; set; }
+    public int Quantity { get; set; }
+    public int Price { get; set; }
+
+    public Chips()
+    {
+    }
+
+    public Chips(string assetPath, int quantity, int price)
+    {
+        Quantity = quantity;
+        Price = price;
+        Image = new Bitmap(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "Chips", assetPath));
+    }
+
+    public void Dispose()
+    {
+        Image?.Dispose();
+    }
+}
--- App/Windows/MainWindow.axaml.cs
+++ App/Windows/MainWindow.axaml.cs
@@ -1,11 +1,45 @@
+using System.Collections.Generic;
+using App.Domain.Models;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Media.Transformation;
+using Microsoft.VisualBasic;
 
 namespace App.Windows;
 
 public partial class MainWindow : Window
 {
+    public List<Chips> Chips { get; set; } =
+    [
+        new("SaltAndVinegar.png", 5, 200),
+        new("SaltAndVinegar.png", 5, 200),
+        new("SaltAndVinegar.png", 5, 200),
+        new("SaltAndVinegar.png", 5, 200),
+        new("SaltAndVinegar.png", 5, 200),
+        new("SaltAndVinegar.png", 5, 200),
+        new("SaltAndVinegar.png", 5, 200),
+        new(),
+        new(),
+        new(),
+        new(),
+        new(),
+        new(),
+        new(),
+        new(),
+        new()
+    ];
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not Button { Content: Panel panel } ) return;
+        
+        panel.ZIndex = 100;
+        panel.Children[1].RenderTransform = TransformOperations.Parse("translate(0px, 200px)");
     }
 }
